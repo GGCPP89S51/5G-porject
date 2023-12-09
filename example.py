e6efb659af5b4e8a56c9fbf166cbfac79d9c6cf2
GGCPP89S51 <92510306+GGCPP89S51@users.noreply.github.com>
2023-12-09 23:50:15 +0800

    Update example.py

--- example.py
+++ example.py
@@ -10,14 +10,16 @@ class Drone_deployment(bd.Feature_value_judgment):
         self.EndPoint = []
         self.file = None
         self.kdtree = None
+        self.serch_radius = 0
 
     def computingHotspots(self,file,DroneSpeed):
 
         self.inputDroneSpeed(DroneSpeed)
         self.inputQuantity(1000)
-        self.inputFeaturesLowest(30)
+        self.inputFeaturesLowest(60)
         self.inputStarttime (0) 
         self.inputEndtime(24)
+        self.serch_radius = self.radius / 10
         
         self.file = file
         df = pd.read_csv(self.file, encoding="utf-8")
@@ -58,10 +60,13 @@ class Drone_deployment(bd.Feature_value_judgment):
         print(len(query_points))
         query_points = cKDTree(query_points)
         # 使用 KD-Tree 查找在1公里內的點
-        indices = self.kdtree.query_ball_tree(query_points, r=0.01)
+        indices = self.kdtree.query_ball_tree(query_points, r=self.serch_radius*0.01)
+        counter = 0
         for i in range( 0 , len(indices)):
             for j in range(0,len(indices[i])) :
                 EndPoint[i][2] += 1
+                counter += 1
+        print(EndPoint)
                 
     def commuting_work_time_analysis(self):
         EndPoint = self.EndPoint
@@ -78,7 +83,7 @@ class Drone_deployment(bd.Feature_value_judgment):
         print(len(query_points))
         query_points = cKDTree(query_points)
         # 使用 KD-Tree 查找在1公里內的點
-        indices = self.kdtree.query_ball_tree(query_points, r=0.01)
+        indices = self.kdtree.query_ball_tree(query_points, r=self.serch_radius*0.01)
         for i in range( 0 , len(indices)):
             for j in range(0,len(indices[i])) :
                 EndPoint[i][2] += 1
@@ -98,7 +103,7 @@ class Drone_deployment(bd.Feature_value_judgment):
         print(len(query_points))
         query_points = cKDTree(query_points)
         # 使用 KD-Tree 查找在1公里內的點
-        indices = self.kdtree.query_ball_tree(query_points, r=0.01)
+        indices = self.kdtree.query_ball_tree(query_points, r=self.serch_radius*0.01)
         for i in range( 0 , len(indices)):
             for j in range(0,len(indices[i])) :
                 EndPoint[i][2] += 1
@@ -118,7 +123,7 @@ class Drone_deployment(bd.Feature_value_judgment):
         print(len(query_points))
         query_points = cKDTree(query_points)
         # 使用 KD-Tree 查找在1公里內的點
-        indices = self.kdtree.query_ball_tree(query_points, r=0.01)
+        indices = self.kdtree.query_ball_tree(query_points, r=self.serch_radius*0.01)
         for i in range( 0 , len(indices)):
             for j in range(0,len(indices[i])) :
                 EndPoint[i][2] += 1
@@ -138,7 +143,7 @@ class Drone_deployment(bd.Feature_value_judgment):
         print(len(query_points))
         query_points = cKDTree(query_points)
         # 使用 KD-Tree 查找在1公里內的點
-        indices = self.kdtree.query_ball_tree(query_points, r=0.01)
+        indices = self.kdtree.query_ball_tree(query_points, r=self.serch_radius*0.01)
         for i in range( 0 , len(indices)):
             for j in range(0,len(indices[i])) :
                 EndPoint[i][2] += 1
@@ -147,7 +152,7 @@ class Drone_deployment(bd.Feature_value_judgment):
 def main():
     file_path = r"臺南市112年上半年道路交通事故原因傷亡統計.csv"
     test = Drone_deployment()
-    test.computingHotspots(file_path,60)
+    test.computingHotspots(file_path,45)
     test.night_time_analysis()
 if __name__=="__main__":
     main()
