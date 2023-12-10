044c132375f2235f555ef6252761a99256be0086
GGCPP89S51 <92510306+GGCPP89S51@users.noreply.github.com>
2023-12-10 21:44:29 +0800

    Update big_data.py

--- big_data.py
+++ big_data.py
@@ -54,7 +54,7 @@ class Feature_value_judgment:
     # 輸入無人機時速
     def inputDroneSpeed(self, speed):
         self.radius = int(speed / 6)
-
+        
     # 輸入開始時間
     def inputStarttime(self, start_time):
         self.start_time = start_time
