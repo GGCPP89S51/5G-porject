3bdef74fdfa626b46ca29ee4a5ab6462ce429384
GGCPP89S51 <92510306+GGCPP89S51@users.noreply.github.com>
2023-12-10 21:44:23 +0800

    Update algorithms_GUI.py

--- algorithms_GUI.py
+++ algorithms_GUI.py
@@ -1,4 +1,4 @@
-from PyQt6 import QtWidgets, QtCore, QtWebEngineWidgets
+from PyQt6 import QtWidgets, QtCore
 import sys, cv2, big_data
 from PyQt6.QtGui import *
 from PyQt6.QtNetwork import QNetworkAccessManager, QNetworkRequest
