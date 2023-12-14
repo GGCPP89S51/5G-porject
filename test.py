5a9895d2a13a65822e6873c158490fb5dbd1312d
GGCPP89S51 <92510306+GGCPP89S51@users.noreply.github.com>
2023-12-14 09:28:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,67 +1,33 @@
-import numpy as np
+import random
 
-def k_means(data, k, max_iterations=100, tolerance=0.001):
-    # 步骤1：初始化聚类中心
-    centroids = np.array(randomly_initialize_centroids(data, k))
-    
-    for iteration in range(max_iterations):
-        # 步骤2：分配数据点到最近的聚类中心
-        clusters = assign_to_clusters(data, centroids)
-        
-        # 步骤3：更新聚类中心
-        new_centroids = update_centroids(clusters)
-        
-        # 检查聚类中心是否变化不大
-        if centroids_converged(centroids, new_centroids, tolerance):
-            break
-        
-        centroids = new_centroids
-    
-    return clusters, centroids
+def objective_function(x, y):
+    return x**2 + y**2
 
-# 辅助函数：初始化聚类中心
-def randomly_initialize_centroids(data, k):
-    indices = np.random.choice(len(data), k, replace=False)
-    return [data[i] for i in indices]
+def bee_algorithm(max_iter, num_bees, elite_sites, patch_size):
+    bees = [{'x': random.uniform(-5, 5), 'y': random.uniform(-5, 5)} for _ in range(num_bees)]
 
-# 辅助函数：分配数据点到最近的聚类中心
-def assign_to_clusters(data, centroids):
-    clusters = [[] for _ in range(len(centroids))]
-    
-    for point in data:
-        distances = [np.linalg.norm(point - centroid) for centroid in centroids]
-        closest_centroid_index = np.argmin(distances)
-        clusters[closest_centroid_index].append(point)
-    
-    return clusters
+    for iteration in range(max_iter):
+        fitness_values = [objective_function(bee['x'], bee['y']) for bee in bees]
+        elite_bees = sorted(range(num_bees), key=lambda k: fitness_values[k])[:elite_sites]
 
-# 辅助函数：更新聚类中心
-def update_centroids(clusters):
-    new_centroids = [np.mean(cluster, axis=0) for cluster in clusters]
-    return new_centroids
+        for elite_bee in elite_bees:
+            new_x = bees[elite_bee]['x'] + random.uniform(-patch_size, patch_size)
+            new_y = bees[elite_bee]['y'] + random.uniform(-patch_size, patch_size)
 
-# 辅助函数：检查聚类中心是否变化不大
-def centroids_converged(old_centroids, new_centroids, tolerance):
-    return all(np.linalg.norm(old - new) < tolerance for old, new in zip(old_centroids, new_centroids))
+            if objective_function(new_x, new_y) < objective_function(bees[elite_bee]['x'], bees[elite_bee]['y']):
+                bees[elite_bee]['x'] = new_x
+                bees[elite_bee]['y'] = new_y
 
-# 主程式
+    best_bee = min(bees, key=lambda bee: objective_function(bee['x'], bee['y']))
+    return best_bee
+
 if __name__ == "__main__":
-    # 生成示例数据（替换成你的实际数据）
-    data = np.random.rand(100, 2)  # 100个二维数据点
-    
-    # 设置K值和其他参数
-    k = 3
     max_iterations = 100
-    tolerance = 0.001
-    
-    # 调用K均值聚类函数
-    clusters, centroids = k_means(data, k, max_iterations, tolerance)
-    
-    # 打印结果或进行其他后续处理
-    print("Final Clusters:")
-    for i, cluster in enumerate(clusters):
-        print(f"Cluster {i + 1}: {cluster}")
+    num_bees = 20
+    elite_sites = 5
+    patch_size = 0.5
 
-    print("\nFinal Centroids:")
-    for i, centroid in enumerate(centroids):
-        print(f"Centroid {i + 1}: {centroid}")
+    best_solution = bee_algorithm(max_iter=max_iterations, num_bees=num_bees, elite_sites=elite_sites, patch_size=patch_size)
+
+    print("最优解：", best_solution)
+    print("目标函数值：", objective_function(best_solution['x'], best_solution['y']))
