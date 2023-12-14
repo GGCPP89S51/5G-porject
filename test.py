dad46108c04006aec2ee96f0ff597920ca6bde22
GGCPP89S51 <92510306+GGCPP89S51@users.noreply.github.com>
2023-12-14 13:39:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,33 +1,39 @@
+import numpy as np
 import random
 
-def objective_function(x, y):
-    return x**2 + y**2
+# 生成随机的需求点
+num_demands = 10
+demand_points = np.random.rand(num_demands, 2) * 100  # 二维空间中的需求点
 
-def bee_algorithm(max_iter, num_bees, elite_sites, patch_size):
-    bees = [{'x': random.uniform(-5, 5), 'y': random.uniform(-5, 5)} for _ in range(num_bees)]
+# 蜂群算法函数
+def bee_algorithm_facility_location(max_iter, num_bees, patch_size):
+    # 初始化蜜蜂群体
+    bees = [{'locations': np.random.rand(2) * 100} for _ in range(num_bees)]
 
     for iteration in range(max_iter):
-        fitness_values = [objective_function(bee['x'], bee['y']) for bee in bees]
-        elite_bees = sorted(range(num_bees), key=lambda k: fitness_values[k])[:elite_sites]
+        # 评估蜜蜂适应度
+        fitness_values = [np.min(np.linalg.norm(bee['locations'] - demand_points, axis=1)) for bee in bees]
 
-        for elite_bee in elite_bees:
-            new_x = bees[elite_bee]['x'] + random.uniform(-patch_size, patch_size)
-            new_y = bees[elite_bee]['y'] + random.uniform(-patch_size, patch_size)
+        # 选择蜜蜂
+        elite_bee = bees[np.argmin(fitness_values)]
 
-            if objective_function(new_x, new_y) < objective_function(bees[elite_bee]['x'], bees[elite_bee]['y']):
-                bees[elite_bee]['x'] = new_x
-                bees[elite_bee]['y'] = new_y
+        # 信息传递和局部搜索
+        new_locations = elite_bee['locations'] + np.random.uniform(-patch_size, patch_size, 2)
 
-    best_bee = min(bees, key=lambda bee: objective_function(bee['x'], bee['y']))
+        # 更新解
+        if np.min(np.linalg.norm(new_locations - demand_points, axis=1)) < np.min(np.linalg.norm(elite_bee['locations'] - demand_points, axis=1)):
+            elite_bee['locations'] = new_locations
+
+    # 返回最优解
+    best_bee = min(bees, key=lambda bee: np.min(np.linalg.norm(bee['locations'] - demand_points, axis=1)))
     return best_bee
 
 if __name__ == "__main__":
     max_iterations = 100
     num_bees = 20
-    elite_sites = 5
-    patch_size = 0.5
+    patch_size = 5.0
 
-    best_solution = bee_algorithm(max_iter=max_iterations, num_bees=num_bees, elite_sites=elite_sites, patch_size=patch_size)
+    best_solution = bee_algorithm_facility_location(max_iter=max_iterations, num_bees=num_bees, patch_size=patch_size)
 
-    print("最优解：", best_solution)
-    print("目标函数值：", objective_function(best_solution['x'], best_solution['y']))
+    print("最优设施位置：", best_solution['locations'])
+    print("目标函数值：", np.min(np.linalg.norm(best_solution['locations'] - demand_points, axis=1)))
