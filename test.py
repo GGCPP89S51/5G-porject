ce5ec8f2aa0169516a942cb41f86e32907eacbfe
GGCPP89S51 <92510306+GGCPP89S51@users.noreply.github.com>
2023-12-18 21:51:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,228 +1,40 @@
-import numpy as np
-import random
-import math
-from random import choice
-import matplotlib.pyplot as plt
+import pandas as pd
 
-#Parameters
-D= 5
-N= 10
-lb = -5
-ub = 5
-Lb= -5*np.ones(D)
-Ub= 5*np.ones(D)
-max_iter= 10000
-limit = 50
+# Your list of coordinates
+coordinates = [
+    [120.201, 22.996, 719.0], [120.215, 22.992, 526.0], [120.231, 22.989, 487.0], [120.221, 23.019, 451.0],
+    [120.193, 22.985, 409.0], [120.21, 23.005, 394.0], [120.226, 23.005, 392.0], [120.191, 23.004, 372.0],
+    [120.221, 22.981, 369.0], [120.233, 23.015, 337.0], [120.207, 22.985, 319.0], [120.238, 23.0, 302.0],
+    [120.232, 22.974, 290.0], [120.209, 23.017, 284.0], [120.246, 23.016, 246.0], [120.181, 22.995, 239.0],
+    [120.294, 23.131, 236.0], [120.241, 23.04, 235.0], [120.256, 23.026, 228.0], [120.178, 23.161, 226.0],
+    [120.314, 23.307, 225.0], [120.263, 23.007, 219.0], [120.19, 22.974, 215.0], [120.196, 23.016, 209.0],
+    [120.21, 23.034, 196.0], [120.251, 22.998, 195.0], [120.294, 23.077, 186.0], [120.168, 22.997, 179.0],
+    [120.306, 23.037, 172.0], [120.25, 23.182, 162.0], [120.21, 22.973, 158.0], [120.242, 22.986, 154.0],
+    [120.201, 23.046, 150.0], [120.232, 23.029, 149.0], [120.192, 23.029, 147.0], [120.251, 22.973, 147.0],
+    [120.293, 22.97, 145.0], [120.219, 23.067, 140.0], [120.263, 23.02, 126.0], [120.19, 22.963, 120.0],
+    [120.252, 23.038, 116.0], [120.235, 22.963, 113.0], [120.183, 23.012, 112.0], [120.224, 22.995, 111.0],
+    [120.188, 23.042, 107.0], [120.189, 22.993, 100.0], [120.331, 22.96, 94.0], [120.176, 23.051, 93.0],
+    [120.24, 23.051, 93.0], [120.322, 23.314, 93.0], [120.266, 23.035, 92.0], [120.3, 23.305, 89.0],
+    [120.216, 23.047, 88.0], [120.176, 22.986, 85.0], [120.218, 23.03, 84.0], [120.307, 23.131, 83.0],
+    [120.228, 23.041, 82.0], [120.221, 22.965, 81.0], [120.199, 23.007, 80.0], [120.254, 22.954, 80.0],
+    [120.263, 22.994, 79.0], [120.183, 23.234, 76.0], [120.231, 23.09, 75.0], [120.204, 23.118, 72.0],
+    [120.224, 22.925, 72.0], [120.276, 23.019, 70.0], [120.183, 22.935, 69.0], [120.219, 23.008, 69.0],
+    [120.242, 23.007, 68.0], [120.281, 23.079, 66.0], [120.287, 23.066, 64.0], [120.17, 23.165, 63.0],
+    [120.202, 23.026, 62.0], [120.211, 23.092, 61.0], [120.27, 23.322, 60.0], [120.228, 22.936, 58.0],
+    [120.181, 23.15, 57.0], [120.243, 22.972, 55.0], [120.272, 23.049, 55.0], [120.278, 22.973, 55.0],
+    [120.35, 23.232, 55.0], [120.238, 23.186, 53.0], [120.29, 23.03, 53.0], [120.416, 23.351, 53.0],
+    [120.272, 23.007, 52.0], [120.277, 22.953, 52.0], [120.311, 23.318, 52.0], [120.268, 23.235, 51.0],
+    [120.199, 22.974, 50.0], [120.218, 23.08, 50.0], [120.277, 22.993, 48.0], [120.16, 23.0, 47.0],
+    [120.24, 22.925, 47.0], [120.301, 23.087, 47.0], [120.283, 22.926, 46.0], [120.187, 23.187, 45.0],
+    [120.259, 22.967, 45.0], [120.299, 23.061, 45.0], [120.311, 23.297, 44.0], [120.173, 23.068, 43.0]
+]
 
-#Rastrigin function
-def fun(X):
-    funsum = 0
-    for i in range(D):
-        x = X[:,i]
-        funsum += x**2 - 10*np.cos(2*np.pi*x)
-    funsum += 10*D
-    return funsum
+# Convert the list to a DataFrame
+df = pd.DataFrame(coordinates, columns=['Longitude', 'Latitude', 'Altitude'])
 
-#Styblinski-Tang function, (-5,5)
-def fun_(X):
-    funsum = 0
-    for i in range(D):
-        x = X[:,i]
-        funsum += x**4-16*(x**2)+5*x
-    funsum *= 0.5
-    return funsum
+# Reshape the DataFrame to a 10x10 grid
+df_grid = df.values.reshape((10, 10, -1))
+df_grid = pd.DataFrame(df_grid.reshape(-1, df_grid.shape[-1]), columns=df.columns)
 
-#1D to 2D with calculate 'fun' function
-def transform1Dto2D_fv(num,D,fun):
-    substitute = np.zeros(D)
-    toCalculate = np.row_stack((num, substitute))
-    testOut = fun(toCalculate)
-    result = np.delete(testOut, -1, axis=0)
-    return result
-
-#Fitness function
-def fitness_machine(num):
-    fitness = 1/(1+num) if num>= 0 else 1+abs(num)
-    return fitness
-
-#Build probability list
-def probability_list(fs, fun):
-    out = fun(fs)
-    fvList = []
-    Probability_list = []
-    for count_fitness in out:
-        fitValue = fitness_machine(count_fitness)
-        fvList.append(fitValue)
-    sum_fv = sum(fvList)
-    for temp in fvList:
-        each_pro = temp/sum_fv
-        Probability_list.append(each_pro)
-    return Probability_list
-
-#Employed Bee phase
-def Employed_update_fs(num_,fs,fitness_list, trial):
-    fs_row = fs[num_].tolist()
-    num = choice(fs_row)
-    cvIndex = fs_row.index(num)
-    
-    waiting_area = np.delete(fs[:,cvIndex],num_)
-    
-    challenge_num = choice(waiting_area)
-    Xnew = num+(random.uniform(-1,1))*(num-challenge_num)
-    if Xnew > ub:
-        Xnew = ub
-    elif Xnew < lb:
-        Xnew = lb
-    fs_list = []
-    for i in range(D):
-        fs_list.append(fs[num_][i])
-    fs_list[cvIndex] = Xnew
-    fs_element = np.asarray(fs_list)
-    
-    #Judgment elements
-    out_ = transform1Dto2D_fv(fs_element,D,fun)
-    new_fitness = fitness_machine(out_)
-    
-    out_compare = transform1Dto2D_fv(fs[num_],D,fun)
-    fitness_compare = fitness_machine(out_compare)
-
-    #Judgment
-    if new_fitness < fitness_compare:
-        fs[num_][cvIndex] = fs[num_][cvIndex]
-        trial[num_] += 1
-    else:
-        fs[num_][cvIndex] = Xnew
-        trial[num_] = 0
-
-    out = fun(fs)
-    fitness= []
-    for i in range(N):
-        fv = fitness_machine(out[i])
-        fitness.append(fv)
-    return fs, out, fitness, trial, num, Xnew
-
-
-#On-looker Bee phase
-def Onlooker_update_fs_step2(num_,fs_step2):
-    #choose vairable Xn
-    numOnlooker = choice(fs_step2[num_])
-    #choose partner
-    fs_step2_row = fs_step2[num_].tolist()
-    partnerIndex = fs_step2_row.index(numOnlooker)
-    waiting_area_ = np.delete(fs_step2[:,partnerIndex],num_)
-    partner = choice(waiting_area_)
-    Xnew_ = numOnlooker + (random.uniform(-1,1))*(numOnlooker - partner)
-    if Xnew_ > ub:
-        Xnew_ = ub
-    elif Xnew_ < lb:
-        Xnew_ = lb
-    fs_list_onlooker = []
-    for i in range(D):
-        fs_list_onlooker.append(fs_step2[num_][i])
-    fs_list_onlooker[partnerIndex] = Xnew_
-    fs_element_ = np.asarray(fs_list_onlooker)
-    
-    #Judgment elements
-    test_out_onlooker = transform1Dto2D_fv(fs_element_,D,fun)
-    new_fitness_ = fitness_machine(test_out_onlooker)
-    
-    out_compare = transform1Dto2D_fv(fs_step2[num_],D,fun)
-    fitness_compare = fitness_machine(out_compare)
-
-    #Judgment
-    if new_fitness_ < fitness_compare:
-        fs_step2[num_][partnerIndex] = fs_step2[num_][partnerIndex]
-        fv_afe[num_] += 1
-    else:
-        fs_step2[num_][partnerIndex] = Xnew_
-        fv_afe[num_] = 0
-    return fs_step2, fv_afe
-    
-#Generate Food Source
-fs=np.zeros((N,D))
-for i in range(N):
-    for j in range(D):
-        fs[i,j] = np.random.uniform(-5,5)
-
-#Count f(x)
-out = fun(fs)
-
-#Count Fitness value
-fitness_list = []
-for i in range(N):
-    fv = fitness_machine(out[i])
-    fitness_list.append(fv)
-
-#Show the Trial    
-trial = np.zeros(N).astype(int)
-
-#Set up the best_volumn
-best_list = []
-Best_Food_Source_list = []
-
-it = 0
-while it <= max_iter:
-    #Start the while
-    #Start the part of Employed Bee
-    for i in range(N):
-        after_emoloyed = Employed_update_fs(i,fs,fitness_list, trial)
-
-    #Give new variable names
-    fs_1 = after_emoloyed[0]
-    fv_afe = after_emoloyed[3]
-    
-    #Build the probability list
-    Probability_list = probability_list(fs_1,fun)
-
-    #Onlooker Bee Phase
-    fs_step2 = after_emoloyed[0]
-
-    for i in range(N):
-        if random.uniform(0,1) < Probability_list[i]:
-            onlooker_out = Onlooker_update_fs_step2(i,fs_step2)
-            fs_step2_2 = onlooker_out[0]
-            fv_afe = onlooker_out[1]
-        else: 
-            fs_step2_2 = fs_step2
-            fv_afe[i] += 1
-
-    #Scout bee phase
-    for j in range(N):
-        if fv_afe[j] > limit:
-            for i in range(D):
-                fs_step2_2[j][i] = random.uniform(-5,5)
-                fv_afe[j] = 0
-                
-    #Store the best answer
-    final_out = fun(fs_step2)
-    best_ = min(final_out)
-    best_list.append(best_)
-    
-    #Cohesion
-    fs = fs_step2_2
-    trial = fv_afe  
-
-    index_best = np.where(best_)
-    index_best = list(final_out).index(best_)
-    Food_Source_best = fs_step2[index_best]
-    Best_Food_Source_list.append(Food_Source_best)
-    it += 1
-    
-min_best_num = min(best_list)
-#arr = list(best_list).index(max_best_num)
-arr = best_list.index(min_best_num)
-Best_FS_ = Best_Food_Source_list[arr] 
-
-
-#print('')
-np.set_printoptions(suppress=True)
-print('Best F(X): ',min_best_num, 'Best food source: ',Best_FS_)
-
-plt.figure(figsize = (15,8))
-plt.xlabel("Iteration",fontsize = 15)
-plt.ylabel("Value",fontsize = 15)
-
-plt.plot(best_list,linewidth = 2, label = "History Value", color = 'r')
-#plt.plot(every_time_value,linewidth = 2, label = "Best value so far", color = 'b')
-plt.legend()
-plt.show()
+# Save to Excel
+df_grid.to_excel('coordinates_grid.xlsx', index=False)
